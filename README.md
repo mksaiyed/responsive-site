73344c151c756cb8ecd81b07b7549c5116a596fd
mksaiyed <mo.kaifbapu@gecg28.ac.in>
2022-01-17 20:01:29 +0530

    Mobile View Added !

---
+++ README.md
@@ -0,0 +1,17 @@
+# Responsive Website
+
+This is Responsive site Assignment and in this i have build Desktop 🖥 and Mobile 📱 Responsive Website.
+
+---
+
+## Demo gif
+
+### Desktop View
+
+![Desktop](Desktop.gif)
+
+---
+
+### Mobile View
+
+![Mobile](Mobile.gif)
--- script.js
+++ script.js
@@ -2,13 +2,43 @@ window.onscroll = function () {
 	myFunction();
 };
 
-var header = document.getElementById("nav-bar");
-var sticky = header.offsetTop;
+var navBar = document.getElementById("nav-bar");
+var logo = document.getElementById("logo");
+var menu = document.getElementById("menu");
+var menuIcon = document.getElementById("menu-icon");
+var sticky = navBar.offsetTop;
 
 function myFunction() {
 	if (window.pageYOffset > sticky) {
-		header.classList.add("sticky");
+		navBar.classList.add("sticky");
+		logo.src = "/Assets/logo-active.png";
+		menuIcon.src = "/Assets/icons/gray_menu_icon.png";
 	} else {
-		header.classList.remove("sticky");
+		navBar.classList.remove("sticky");
+		logo.src = "/Assets/logo.png";
+		menuIcon.src = "/Assets/icons/white_menu_icon.png";
+	}
+}
+
+function on_menu_icon_click() {
+	menu.style.display = "flex";
+	navBar.style.padding = "0";
+}
+
+function on_up_icon_click() {
+	menu.style.display = "none";
+	navBar.style.padding = "20px 0px";
+}
+
+window.onresize = reportWindowSize;
+
+function reportWindowSize() {
+	// const heightOutput = document.querySelector("#height");
+	// const widthOutput = document.querySelector("#width");
+
+	if (window.innerWidth > 900) {
+		menu.style.display = "flex";
+	} else {
+		menu.style.display = "none";
 	}
 }
